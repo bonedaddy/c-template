788027bbfe541d0cf0884925e8b8eae8b66ee1b2
postables <postables@rtradetechnologies.com>
2020-06-28 18:49:10 -0700

    src/utils: cleanup get_random_string and remove VLA

--- src/utils/random.c
+++ src/utils/random.c
@@ -3,21 +3,16 @@
 #include "../../include/utils/random.h"
 
 char *get_random_string(int stringLength) {
-    // create an array of chars of length stringLength
-    char word[stringLength];
-    for (int i = 0; i < stringLength; i++) {
-        word[i] = letters[get_random_number(0, 25)];
-    }
     // allocate a chunk of memory sized to the word array
     // not sure why its showing an error in vscode???
     // https://stackoverflow.com/questions/50557000/complie-error-in-c-program-visual-studio-regarding-malloc-a-value-of-type-vo?rq=1
-    char *retWord = malloc(sizeof(word));
+    char *retWord = malloc(stringLength);
     if (retWord == NULL) {
         return NULL;
     }
     // assign each char from word to its corresponding vlaue in retWord
     for (int i = 0; i < stringLength; i++) {
-        retWord[i] = word[i];
+        retWord[i] = letters[get_random_number(0, 25)];
     }
     // return a pointer to retWord
     // i dont entirely understand why compiler complains
