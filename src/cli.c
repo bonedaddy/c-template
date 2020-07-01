60af4a4f227e54a1c4533d90cba6c9df9afc91dc
postables <postables@rtradetechnologies.com>
2020-06-30 22:56:48 -0700

    src/cli: switch callback to use argc argv

--- src/cli.c
+++ src/cli.c
@@ -18,7 +18,7 @@ struct command;
 /*
   declares the command handler callback which takes in an instance of command
 */
-typedef void (* command_handler_callback)(struct command *self);
+typedef void (* command_handler_callback)(int argc, char *argv[]);
 
 /*
   command_handler is an individual command
@@ -43,8 +43,12 @@ bool is_flag_argument(char *arg);
 command_object *new_command_object();
 // a command to generate a new zlog configuration
 command_handler *new_zlog_config_command(command_object *self);
-void new_logger_config_wrapper();
-
+// displays the help command
+command_handler *new_help_command(command_object *self);
+// wrapper function to use as the calback
+void new_logger_config_wrapper(int argc, char *argv[]);
+// wrapper function to use as the callbacK
+void new_help_command_wrapper(int argc, char *argv[]);
 
 // NOTE: you will want to add additional statements for any other commands that are being included
 void print_help() {
@@ -52,18 +56,36 @@ void print_help() {
   printf("new-zlog-config <path-to-config> (generate a new zlog config file)\n");
 }
 
-void new_logger_config_wrapper(command_object *self) {
-  char *config_path = self->command->argv[0];
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void new_help_command_wrapper(int argc, char *argv[]) {
+  print_help();
+}
+
+void new_logger_config_wrapper(int argc, char *argv[]) {
+  char *config_path;
+  if (argc == 1) {
+    config_path = malloc(sizeof(argv[0]));
+    config_path = argv[0];
+  }
   if (config_path == NULL) {
+    printf("no config path given defaulting to zlog.conf\n");
     config_path = "zlog.conf";
   }
-  printf("saving zlog config to %s\n", config_path);
+  printf("zlog config saving to %s\n", config_path);
   int response = new_logger_config(config_path);
   if (response != 0) {
     printf("failed call new_logger_config");
   }
-  // TODO(bonedaddy): use dyanmic argument
-  printf("saved zlog config to %s\n", config_path);
+  printf("zlog config saved to %s\n", config_path);
+}
+
+
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+command_handler *new_help_command(command_object *self) {
+    // allocate size of command_handler and the new-log-config `char *`
+  command_handler *handler = malloc(sizeof(command_handler) + sizeof("new-zlog-config"));
+  handler->callback = new_help_command_wrapper;
+  return handler;
 }
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
@@ -76,6 +98,7 @@ command_handler *new_zlog_config_command(command_object *self) {
 }
 // checks whether or not the provide arg is a command line flag
 // it does this by getting the first 2 char's and comparing that to `--`
+#pragma GCC diagnostic ignored "-Wunused-function"
 bool is_flag_argument(char *arg) {
     if (strlen(arg) > 2) {
       char c[2] = {arg[0], arg[1]};
@@ -99,15 +122,6 @@ command_object *new_command_object(int argc, char *argv[]) {
   command_object *pcobj = (command_object *)malloc(sizeof(command_object) + sizeof(*argv));
   // parse cli arguments provided by user 
   for (int i = 0; i < argc; i++) {
-    // we only accept whole commands or flag with `--argName`
-    /* if (strlen(argv[i]) >= 2) {
-    // check to see if arg is a command line flag
-     /if (is_flag_argument(argv[i])) {
-        // TODO(bonedaddy): handle
-        printf("found flag: %s\n", argv[i]);
-      }
-    }
-    */
     pcobj->argv[i] = argv[i];
     pcobj->argc = argc;
   }
@@ -117,20 +131,30 @@ command_object *new_command_object(int argc, char *argv[]) {
     // next argument should be the path to the config file
     if (strcmp(argv[i], "new-zlog-config") == 0) {
       int idx = i + 1;
+      char *config_path;
       if (argc < (idx+1)) {
-        printf("invalid number of arguments provided. got %i want %i\n", argc, idx+1);
-        return NULL;
+        config_path = "zlog.conf";
+      } else {
+        config_path = argv[idx];
       }
-      char *config_path = argv[idx];
+      // NOTE(bonedaddy): looks like this isn't being properly set and passed through to callback
       pcobj->argv[0] = config_path;
+      pcobj->argc = 1;
       printf("config path %s\n", pcobj->argv[0]);
       command_handler *zlg = new_zlog_config_command(pcobj);
       command_handler *zlog_command = malloc(sizeof(zlg));
       zlog_command = zlg;
       pcobj->command = zlog_command;
     }
-/*    ADD YOUR NEW COMMAND CHECKS HERE
-*/
+    // handle help command (triggered via `help` or no arguments)
+    if (strcmp(argv[i], "help") == 0 || argc == 1) {
+      command_handler *hlp = new_help_command(pcobj);
+      command_handler *help_command = malloc(sizeof(hlp));
+      help_command = hlp;
+      pcobj->command = help_command;
+    }
+    /*    ADD YOUR NEW COMMAND CHECKS HERE
+    */
   }
   return pcobj;
 }
@@ -141,5 +165,5 @@ int main(int argc, char *argv[]) {
     printf("failed to get command_object");
     return - 1;
   }
-  pcmd->command->callback(pcmd);
+  pcmd->command->callback(pcmd->argc, pcmd->argv);
 }
