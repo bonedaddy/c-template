36ef7eeb4dff17ad96727d265c9544ecfcda3919
postables <postables@rtradetechnologies.com>
2020-06-30 23:12:43 -0700

    src: integrate safemem with cli

--- src/cli.c
+++ src/cli.c
@@ -6,6 +6,7 @@
 #include "../include/cli.h"
 #include "../include/utils/array_len.h"
 #include "../include/utils/logger.h"
+#include "../include/utils/safe_mem.h"
 
 void print_help() {
   printf("CLI HELP MENU\n-------------\n\n");
@@ -127,5 +128,13 @@ int main(int argc, char *argv[]) {
     printf("failed to get command_object");
     return - 1;
   }
+  // create a safe memory wrapper around the command object
+  memory_object memobj = new_memory_object(pcmd);
   pcmd->command->callback(pcmd->argc, pcmd->argv);
+  int response = free_memory_object(&memobj);
+  if (response != 0) {
+    printf("failed to free memory object\n");
+    return response;
+  }
+  return 0;
 }
