c46a934590040aabb7ec18a34a8c0cec799f10a9
postables <postables@rtradetechnologies.com>
2020-06-30 23:55:39 -0700

    src/utils: adjust command_line test

--- src/utils/command_line_test.c
+++ src/utils/command_line_test.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -8,13 +9,15 @@
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
-void test_print_help(void **state) {
-    print_help();
+void test_is_flag_argument(void **state) {
+    char *arg = "--config";
+    bool is_flag = is_flag_argument(arg);
+    assert(is_flag == true);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_print_help),
+        cmocka_unit_test(test_is_flag_argument),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
