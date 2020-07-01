ac070da92ab8dfe329756a85a34b6b010bf7705d
postables <postables@rtradetechnologies.com>
2020-07-01 00:02:32 -0700

    src/utils: add full command_line test

--- src/utils/command_line_test.c
+++ src/utils/command_line_test.c
@@ -10,14 +10,35 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
 void test_is_flag_argument(void **state) {
-    char *arg = "--config";
-    bool is_flag = is_flag_argument(arg);
+    bool is_flag = is_flag_argument("--config");
     assert(is_flag == true);
+    is_flag = is_flag_argument("l");
+    assert(is_flag == false);
+    is_flag = is_flag_argument("ll");
+    assert(is_flag == false);
+}
+
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+void test_new_command_object(void **state) {
+    int argc = MAX_COMMAND_ARGS;
+    // increase argc beyond the maximum limit
+    argc++;
+    char *argv[1] = {"test_arg"};
+    command_object *cmdobj = new_command_object(argc, argv);
+    assert(cmdobj == NULL);
+    argc = 1;
+    cmdobj = new_command_object(argc, argv);
+    assert(cmdobj != NULL);
+    assert(cmdobj->argc == 1);
+    assert(strcmp(cmdobj->argv[0], "test_arg") == 0);
+    free(cmdobj);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_is_flag_argument),
+        cmocka_unit_test(test_new_command_object),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
