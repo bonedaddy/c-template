e01438a00c65518d3ee9f2620d8da8ccfe02b882
postables <postables@rtradetechnologies.com>
2020-07-04 19:27:33 -0700

    add function to write colored text to FILE*

--- src/utils/colors.c
+++ src/utils/colors.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "../../include/utils/colors.h"
 
 char *get_ansi_color_scheme(COLORS color) {
@@ -36,4 +37,34 @@ char *format_colored(COLORS color, char *message) {
 
 void print_colored(COLORS color, char *message) {
   printf("%s%s%s\n", get_ansi_color_scheme(color), message, ANSI_COLOR_RESET);
+}
+
+int write_colored(FILE *fh, COLORS color, char *message, bool do_flush) {
+    char *pcolor = get_ansi_color_scheme(color);
+    char *reset = get_ansi_color_scheme(COLORS_RESET);
+    // 2 for \n
+    char *write_message = malloc(strlen(pcolor) + strlen(reset) + strlen(message) + 2);
+    strcat(write_message, pcolor);
+    strcat(write_message, message);
+    strcat(write_message, reset);
+    strcat(write_message, "\n");
+    int response = fputs(write_message, fh);
+    if (response != 1) {
+        printf("failed to call fputs: %i\n", response);
+        free(write_message);
+        return 1; // return 1 as we return 0 if there was no error
+    }
+    // FILE* likely buffers internally so this likely doesnt cause any issues
+    // if you want to be safe set do_flush to true, but this will increase disk activity
+    if (do_flush == false) {
+        free(write_message);
+        return response;
+    }
+    // TODO(bonedaddy): should we buffer logs?
+    response = fflush(fh);
+    if (response != 0) {
+        printf("failed to call fflush\n");
+    }
+    free(write_message);
+    return response;
 }
