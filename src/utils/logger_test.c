53e6489e0a465cbd93efe8784958dcb867683467
postables <postables@rtradetechnologies.com>
2020-07-04 22:34:29 -0700

    src/utils: test logger by using pthread

--- src/utils/logger_test.c
+++ src/utils/logger_test.c
@@ -6,17 +6,34 @@
 #include <setjmp.h>
 #include <cmocka.h>
 #include <assert.h>
+#include <pthread.h>
 #include "../../include/utils/logger.h"
+
+
+void *test_log(void *data) {
+    thread_logger *thl = (thread_logger *)data;
+    thl->log_fn(thl, "this is an info log", LOG_LEVELS_INFO);
+    thl->log_fn(thl, "this is a warn log", LOG_LEVELS_WARN);
+    thl->log_fn(thl, "this is an error log", LOG_LEVELS_ERROR);
+    thl->log_fn(thl, "this is a debug log", LOG_LEVELS_DEBUG);
+    pthread_exit(NULL);
+    return NULL;
+}
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 void test_thread_logger(void **state) {
-    // TODO(bonedaddy): should we test with debug disabled?
-    // TODO(bonedaddy): should we run tests using actual pthreads?
     thread_logger *thl = new_thread_logger(true);
     thl->log_fn(thl, "this is an info log", LOG_LEVELS_INFO);
     thl->log_fn(thl, "this is a warn log", LOG_LEVELS_WARN);
     thl->log_fn(thl, "this is an error log", LOG_LEVELS_ERROR);
     thl->log_fn(thl, "this is a debug log", LOG_LEVELS_DEBUG);
+    pthread_t threads[4];
+    for (int i = 0; i < 4; i++) {
+        pthread_create(&threads[i], NULL, test_log, thl);
+    }
+    for (int i = 0; i < 4; i++) {
+        pthread_join(threads[i], NULL);
+    }
     free(thl);
 }
 
