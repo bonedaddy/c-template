21f144ec403179e58302fb8155537374bd8ad8b7
postables <postables@rtradetechnologies.com>
2020-07-07 23:52:59 -0700

    chore: add wip socket server tool

--- src/network/socket.c
+++ src/network/socket.c
@@ -13,31 +13,88 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 #include "../../include/utils/logger.h"
 
-#define ISVALIDSOCKET(s) ((s) >= 0)
-#define CLOSESOCKET(s) close(s)
-#define SOCKET int
-#define GETSOCKETERRNO() (errno)
+typedef struct addrinfo addr_info;
+typedef struct sockaddr sock_addr;
+typedef struct sockaddr_storage sock_addr_storage;
 
 typedef struct socket_server {
     int socket_number;
-    thread_logger *log;
+    log_fn log; // alis for thl->log
+    log_fnf logf; // alias for thl->logf
+    thread_logger *thl;
 } socket_server;
 
+typedef struct client_conn {
+    int socket_number;
+    sock_addr_storage *address;
+} client_conn;
+
+addr_info default_hints();
+socket_server *new_socket_server(addr_info hints, thread_logger *thl, int max_conns, char *port);
+client_conn *accept_client_conn(socket_server *srv);
+
+char  *get_name_info(sock_addr *client_address);
 void print_and_exit(int error_number);
-// socket_server *new_socket_server(struct addinfo hints);
+void close_client_conn(client_conn *conn);
+void close_socket_server(socket_server *srv);
+
+void close_client_conn(client_conn *conn) {
+    close(conn->socket_number);
+}
+void close_socket_server(socket_server *srv) {
+    close(srv->socket_number);
+    clear_thread_logger(srv->thl);
+    free(srv);
+}
 
 void print_and_exit(int error_number) {
     printf("failure detected: %s\n", strerror(error_number));
     exit(-1);
 }
 
-int main(void) {
-    struct addrinfo hints;
-    struct addrinfo *bind_address;
-    int listen_socket_num;
-    // sets hints to all 0
+client_conn *accept_client_conn(socket_server *srv) {
+    sock_addr_storage addr_temp;
+    socklen_t client_len = sizeof(addr_temp);
+    int client_socket_num = accept(
+        srv->socket_number,
+        (sock_addr *)&addr_temp,
+        &client_len
+    );
+    if (client_socket_num < 0) {
+        return NULL;
+    }
+    sock_addr_storage *client_address = malloc(sizeof(addr_temp));
+    client_address = &addr_temp;
+    client_conn *connection = malloc(sizeof(client_conn));
+    if (client_address == NULL) {
+        return NULL;
+    }
+    connection->address = client_address;
+    connection->socket_number = client_socket_num;
+    return connection;
+}
+
+char  *get_name_info(sock_addr *client_address) {
+    char address_info[256];
+    getnameinfo(
+        client_address,
+        sizeof(*client_address),
+        address_info, // output buffer
+        sizeof(address_info), // size of the output buffer
+        0, // second buffer which outputs service name
+        0, // length of the second buffer
+        NI_NUMERICHOST    // want to see hostnmae as an ip address
+    );
+    char *addr = malloc(sizeof(address_info));
+    strcpy(addr, address_info);
+    return addr;
+}
+
+addr_info default_hints() {
+    addr_info hints;
     memset(&hints, 0, sizeof(hints));
     // change to AF_INET6 to use IPv6
     hints.ai_family = AF_INET;
@@ -45,84 +102,92 @@ int main(void) {
     hints.ai_socktype = SOCK_STREAM;
     // indicates to getaddrinfo we want to bind to the wildcard address
     hints.ai_flags = AI_PASSIVE;
-    // hints allows speicfying the information we are looking for
-    getaddrinfo(0, "8081", &hints, &bind_address);
-    // generate the actual socket
-    listen_socket_num = socket(
+    return hints;
+}
+
+socket_server *new_socket_server(addr_info hints, thread_logger *thl, int max_conns, char *port) {
+    addr_info *bind_address;
+    int rc = getaddrinfo(0, port, &hints, &bind_address);
+    if (rc != 0) {
+        thl->log(thl, 0, "failed to getaddrinfo", LOG_LEVELS_ERROR);
+        return NULL;
+    }
+    int listen_socket_num = socket(
         bind_address->ai_family,
         bind_address->ai_socktype,
-        bind_address->ai_protocol    
+        bind_address->ai_protocol
     );
-    if (listen_socket_num < 0) { // could also check against errno
-        printf("failed to create socket\n");
-        return -1;
+    if (listen_socket_num < 0) {
+        thl->log(thl, 0, "socket creation failed", LOG_LEVELS_ERROR);
+        return NULL;
     }
     bind(
-        listen_socket_num, 
-        bind_address->ai_addr, 
+        listen_socket_num,
+        bind_address->ai_addr,
         bind_address->ai_addrlen
     );
     if (errno != 0) {
-        print_and_exit(errno);
+        thl->logf(thl, 0, LOG_LEVELS_ERROR, "socket bind failed with error %s", strerror(errno));
+        return NULL;
     }
-    // free resources allocated to bind_address
+    // free up addrinfo resources
     freeaddrinfo(bind_address);
-    // start accepting up to N connections
-    listen(listen_socket_num, /*max cons*/ 100);
+    // start listening
+    listen(listen_socket_num, max_conns);
     if (errno != 0) {
-        print_and_exit(errno);
+        thl->logf(thl, 0, LOG_LEVELS_ERROR, "failed to start listening on socket with error %s", strerror(errno));
+        return NULL;
     }
-    // stores the address info for the connecting client
-    struct sockaddr_storage client_address;
-    socklen_t client_len = sizeof(client_address);
-    // blocks until a new connection is made
-    // and when connection is made we create a new socket for it
-    // the returned socket number will be usable to send and receive data over
-    int client_socket_num = accept(
-        listen_socket_num,
-        (struct sockaddr*)&client_address,
-        &client_len // specifies the size of the buffer we're passing in
-    );
-    if (client_socket_num < 0) {
-        printf("failed to create client socket\n");
+    socket_server *srv = calloc(sizeof(socket_server), sizeof(socket_server));
+    if (srv == NULL) {
+        thl->log(thl, 0,"socket_srv malloc failed", LOG_LEVELS_ERROR);
+    }
+    srv->socket_number = listen_socket_num;
+    srv->thl = thl;
+    srv->log = thl->log;
+    srv->logf = thl->logf;
+    srv->log(thl, 0, "socket server creation succeeded", LOG_LEVELS_INFO);
+    return srv;
+}
+
+int main(void) {
+    thread_logger *thl = new_thread_logger(false);
+    addr_info hints = default_hints();
+    socket_server *srv = new_socket_server(hints, thl, 100, "8081");
+    if (srv == NULL) {
+        srv->log(thl, 0, "socket server creation failed", LOG_LEVELS_ERROR);
         return -1;
     }
-    char address_buffer[100];
-    getnameinfo(
-        (struct sockaddr*)&client_address, 
-        client_len, 
-        address_buffer, // output buffer
-        sizeof(address_buffer), // size of the output buffer
-        0, // second buffer which outputs service name
-        0, // length of the second buffer
-        NI_NUMERICHOST    // want to see hostnmae as an ip address
-    );
-    printf("%s\n", address_buffer);
+    client_conn *conn = accept_client_conn(srv);
+    if (conn == NULL) {
+        srv->log(thl, 0, "failed to accept client connection", LOG_LEVELS_ERROR);
+        return -1;
+    }
+    char *address_buffer = get_name_info((sock_addr *)conn->address);
+    thl->logf(thl, 0, LOG_LEVELS_INFO, "new client connected: %s", address_buffer);
     char request[1024];
     for (;;) {
         int bytes_received = recv(
-            client_socket_num, 
+            conn->socket_number, 
             request, // output buffer
             1024,  // output buffer size
             0 // speciifes flags
         );
         if (bytes_received == 0 || bytes_received == -1) {
-            printf("client disconnected\n");
+            srv->log(srv->thl, 0, "client disconnected", LOG_LEVELS_INFO);
             break;
         }
         int bytes_sent = send(
-            client_socket_num,
+            conn->socket_number,
             request,
             strlen(request),
             0 // specifies flags
         );
-        if (bytes_sent < strlen(request)) {
-            // production: send the reqest of the data
-            printf("sent less bytes than execpted\n");
+        if ((size_t)bytes_sent < strlen(request)) {
+            srv->log(srv->thl, 0, "sent less bytes than expected", LOG_LEVELS_WARN);
         }
-        printf("bytes sent: %i\n", bytes_sent);
     }
     // close allocated sockets
-    close(listen_socket_num);
-    close(client_socket_num);
+    close_socket_server(srv);
+    close_client_conn(conn);
 } 
