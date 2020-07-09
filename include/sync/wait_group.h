ecfc8a507d0e1504b8109ef5f4f6904e3ff1085e
postables <postables@rtradetechnologies.com>
2020-07-09 01:20:13 -0700

    include/sync: cleanup wait_Group header file

--- include/sync/wait_group.h
+++ include/sync/wait_group.h
@@ -1,16 +1,17 @@
 /*! @file wait_group.h
   * @author Bonedaddy
-  * @brief provides golang like sync.Wait functionality
+  * @brief provides C equivalent of Golang sync.Wait
   * allows management of multiple threads, and enabling clean exits or cleanups
+  * it is a bit different than joining pthreads to wait for processes to exit
+  * as it doesn't require attached pthreads (can be used with detached) for synchronizatiion
+  * @todo:
+  *   - determine if this is more efficient than using thread joining
 */
 
 #pragma once
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdatomic.h>
 #include <pthread.h>
-#include <unistd.h>
 
 /*! @typedef wait_group_t
   * @brief a simple structure of a pthread mutex and integer counter
@@ -25,6 +26,7 @@ typedef struct wait_group_t {
   * @param count the number of new active processes
 */
 void wait_group_add(wait_group_t *wg, int count);
+
 /*! @brief used by a process to indicate it is done, decreasing the active process counter
   * we include a return code here to catch errors when trying to decrement count below 0
   * @pre wg->active_process must be greater than or equal to 1 otherwise return -1
@@ -34,11 +36,13 @@ void wait_group_add(wait_group_t *wg, int count);
   * @return Success: 0
 */
 int wait_group_done(wait_group_t *wg);
+
 /*! @brief used to wait until current active_processes reaches 0, polling every 0.75 seconds
   * will run free on the wait_group_t pointer to clear up resources
   * @param wg the waitgroup to wait on
 */
 void wait_group_wait(wait_group_t *wg);
+
 /*! @brief returns a new and initialized wait_group_t pointer
 */
 wait_group_t *wait_group_new();
